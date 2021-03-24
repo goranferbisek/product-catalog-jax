69d1dc9c1219737c815d2b65c28ac90e96116d95
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-03-24 17:42:02 +0100

    Initialize a JAX-RS rest api with a resource path

---
+++ src/main/java/si/goranferbisek/ProductCatalogRestApplication.java
@@ -0,0 +1,9 @@
+package si.goranferbisek;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/catalog/api")
+public class ProductCatalogRestApplication extends Application {
+
+}
