aed857581ab83fe14da5ddb35a8bff42f1cd6369
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-03-28 16:42:22 +0200

    Implement CRUD methods for the API

--- src/main/java/si/rest/InventoryItemEndpoint.java
+++ src/main/java/si/rest/InventoryItemEndpoint.java
@@ -1,8 +1,14 @@
 package si.rest;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -14,6 +20,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
 
 import si.goranferbisek.InventoryItem;
 
@@ -22,46 +29,67 @@ import si.goranferbisek.InventoryItem;
 @Produces("application/json")
 @Consumes("application/json")
 public class InventoryItemEndpoint {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
+	@Transactional
 	@POST
 	public Response create(final InventoryItem inventoryitem) {
-		//TODO: process the given inventoryitem 
-		//you may want to use the following return statement, assuming that InventoryItem#getId() or a similar method 
-		//would provide the identifier to retrieve the created InventoryItem resource:
-		//return Response.created(UriBuilder.fromResource(InventoryItemEndpoint.class).path(String.valueOf(inventoryitem.getId())).build()).build();
-		return Response.created(null).build();
+		this.entityManager.persist(inventoryitem);
+		return Response.created(
+				UriBuilder.fromResource(InventoryItemEndpoint.class)
+					.path(String.valueOf(inventoryitem.getInventoryItemId())).build()
+				).build();
 	}
 
 	@GET
 	@Path("/{id:[0-9][0-9]*}")
 	public Response findById(@PathParam("id") final Long id) {
-		//TODO: retrieve the inventoryitem 
-		InventoryItem inventoryitem = new InventoryItem(1L, 1L, "Dell monitor", 10L);
+		InventoryItem inventoryitem = this.entityManager.find(InventoryItem.class, id);
+		
 		if (inventoryitem == null) {
 			return Response.status(Status.NOT_FOUND).build();
 		}
+		
+		inventoryitem.setQuantity(ThreadLocalRandom.current().nextLong(1, 100));
 		return Response.ok(inventoryitem).build();
+	}
+	
+	@GET
+	@Path("/catalog")
+	public InventoryItem findByCatalogId(@NotNull @QueryParam("catalogItemId") Long catalogItemId) {
+		TypedQuery<InventoryItem> query = this.entityManager
+				.createQuery("select i from InventoryItem i where i.catalogItemId = :catalogItemId", InventoryItem.class)
+				.setParameter("catalogItemId", catalogItemId);
+		
+		InventoryItem item = query.getSingleResult();
+		item.setQuantity(ThreadLocalRandom.current().nextLong(1,100));
+	
+		return item; 
 	}
 
 	@GET
 	public List<InventoryItem> listAll(@QueryParam("start") final Integer startPosition,
 			@QueryParam("max") final Integer maxResult) {
-		//TODO: retrieve the inventoryitems 
-		final List<InventoryItem> inventoryitems = null;
+		TypedQuery<InventoryItem> query = this.entityManager.createQuery("select i from InventoryItem i", InventoryItem.class);
+		final List<InventoryItem> inventoryitems = query.getResultList(); 
 		return inventoryitems;
 	}
 
+	@Transactional
 	@PUT
 	@Path("/{id:[0-9][0-9]*}")
 	public Response update(@PathParam("id") Long id, final InventoryItem inventoryitem) {
-		//TODO: process the given inventoryitem 
+		this.entityManager.merge(inventoryitem);
 		return Response.noContent().build();
 	}
 
+	@Transactional
 	@DELETE
 	@Path("/{id:[0-9][0-9]*}")
 	public Response deleteById(@PathParam("id") final Long id) {
-		//TODO: process the inventoryitem matching by the given id 
+		this.entityManager.remove(this.entityManager.find(InventoryItem.class,id));
 		return Response.noContent().build();
 	}
 
